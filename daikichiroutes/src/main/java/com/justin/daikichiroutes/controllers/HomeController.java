a6c97b327e4cb35d20e7a50b138a49f3f9793c09
Justin Lau <justinwlau@gmail.com>
2023-03-12 14:55:54 -0700

    daikichi path

--- daikichiroutes/src/main/java/com/justin/daikichiroutes/controllers/HomeController.java
+++ daikichiroutes/src/main/java/com/justin/daikichiroutes/controllers/HomeController.java
@@ -2,6 +2,7 @@ package com.justin.daikichiroutes.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/daikichi")
@@ -19,5 +20,18 @@ public class HomeController {
 	@RequestMapping("/tomorrow")
 	public String tomorrow() {
 		return "Tomorrow, an opportunity will arise, so be sure to be open to new ideas!";
+	}
+	
+	@RequestMapping("/travel/{destination}")
+	public String travel(@PathVariable("destination") String destination) {
+		return "Congratulations! You will soon travel to " + destination + "!";
+	}
+	
+	@RequestMapping("/lotto/{lottoNumber}")
+	public String lotto(@PathVariable("lottoNumber") Integer lottoNumber) {
+		if (lottoNumber % 2 == 0) {
+			return "You will take a grand journey in the near future, but be wary of tempting offers.";
+		}
+		return "You have enjoyed the fruits of your labor but now is a great time to spend time with family and friends.";
 	}
 }
